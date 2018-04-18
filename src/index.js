2a7e2288ad18e451ce5bd17a371bd76f65c24fed
Jam Risser <jam@jamrizzi.com>
2018-04-18 14:56:19 +0200

    Added ignore

--- src/index.js
+++ src/index.js
@@ -2,21 +2,32 @@ import _ from 'lodash';
 import loadConf from 'load-conf';
 import path from 'path';
 
-export default function rcConfig(name) {
-  let config = _.merge(
-    config,
-    loadConf(path.resolve(`.${name}rc.yaml`), 'yaml')
-  );
-  config = _.merge(config, loadConf(path.resolve(`.${name}rc.yml`), 'yaml'));
-  config = _.merge(config, loadConf(path.resolve(`.${name}rc.json`), 'json'));
-  config = _.merge(
-    config,
-    loadConf(path.resolve(`.${name}rc.js`), 'javascript')
-  );
-  config = _.merge(config, loadConf(path.resolve(`.${name}rc`)));
-  config = _.merge(
-    config,
-    loadConf(path.resolve('package.json'), 'json')[name]
-  );
+export default function rcConfig(name, ignore = []) {
+  if (!_.isArray(ignore)) ignore = [ignore];
+  let config = {};
+  if (!_.includes(ignore, 'yaml')) {
+    config = _.merge(config, loadConf(path.resolve(`.${name}rc.yaml`), 'yaml'));
+  }
+  if (!_.includes(ignore, 'yml')) {
+    config = _.merge(config, loadConf(path.resolve(`.${name}rc.yml`), 'yaml'));
+  }
+  if (!_.includes(ignore, 'json')) {
+    config = _.merge(config, loadConf(path.resolve(`.${name}rc.json`), 'json'));
+  }
+  if (!_.includes(ignore, 'js')) {
+    config = _.merge(
+      config,
+      loadConf(path.resolve(`.${name}rc.js`), 'javascript')
+    );
+  }
+  if (!_.includes(ignore, 'rc')) {
+    config = _.merge(config, loadConf(path.resolve(`.${name}rc`)));
+  }
+  if (!_.includes(ignore, 'package.json')) {
+    config = _.merge(
+      config,
+      loadConf(path.resolve('package.json'), 'json')[name]
+    );
+  }
   return config;
 }
--- tests/index.js
+++ tests/index.js
@@ -5,4 +5,12 @@ describe('rcConfig', () => {
     const config = rcConfig('rcconfig');
     expect(config).toEqual({ howdy: 'texas', hello: 'world' });
   });
+  it('should ignore ignore item', async () => {
+    const config = rcConfig('rcconfig', 'package.json');
+    expect(config).toEqual({ hello: 'world' });
+  });
+  it('should ignore ignore list', async () => {
+    const config = rcConfig('rcconfig', ['package.json', 'rc']);
+    expect(config).toEqual({});
+  });
 });
